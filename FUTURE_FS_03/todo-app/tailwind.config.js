8c43ac090d7f52b87c01ab268b550f749eabe7ad
JoanChege <jncheg@gmail.com>
2024-11-20 15:31:47 +0300

    add tailwind css forms plugins

--- FUTURE_FS_03/todo-app/tailwind.config.js
+++ FUTURE_FS_03/todo-app/tailwind.config.js
@@ -1,17 +1,22 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        pink: {
+          DEFAULT: '#FF69B4',
+          light: '#FFC0CB',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
