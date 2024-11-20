78cab32b3562557b120f354e896f59a435561ed1
JoanChege <jncheg@gmail.com>
2024-11-20 17:57:15 +0300

    add navbar

---
+++ FUTURE_FS_03/todo-list-app/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <div v-if="username">
+            Welcome, {{ username }}
+        </div>
+        <div v-else>
+            <router-link to="/login">Login</router-link>
+            <router-link to="/register">Register</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            username: localStorage.getItem('username'),
+        };
+    };
+};
+</script>
