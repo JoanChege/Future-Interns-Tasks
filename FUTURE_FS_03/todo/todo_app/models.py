e65c0d99f5a34291fd56ffa9695cc1b8bed4bf86
JoanChege <jncheg@gmail.com>
2024-11-21 19:58:36 +0300

    add models

--- FUTURE_FS_03/todo/todo_app/models.py
+++ FUTURE_FS_03/todo/todo_app/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    is_complete = models.BooleanField(default=False)
+    due_date = models.DateField()
+    notes = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.title
