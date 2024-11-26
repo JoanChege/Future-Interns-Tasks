6f305bfd1f410c0f0a19b0ffc1d21cb5d637d6c7
JoanChege <jncheg@gmail.com>
2024-11-26 19:50:51 +0300

    add

--- FUTURE_FS_03/backend/todo_project/todo_app/models.py
+++ FUTURE_FS_03/backend/todo_project/todo_app/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link tasks to a user
+    title = models.CharField(max_length=200)
+    due_date = models.DateField()
+    notes = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
